ef1ae7700d04f680ef44296bc6325df645bc4ec2
michealarumgbo <arumgbomicheal@gmail.com>
2025-09-24 15:49:15 +0100

    Edited some files and added members page

--- osusu-frontend/src/router/index.js
+++ osusu-frontend/src/router/index.js
@@ -4,28 +4,33 @@ const routeList = [
   {
     path: '/',
     name: 'Guest',
-    component: () => import ('@/views/Guest.vue')
+    component: () => import('@/views/Guest.vue'),
   },
   {
     path: '/Login',
     name: 'Login',
-    component: () => import ('@/views/Login.vue')
+    component: () => import('@/views/Login.vue'),
   },
   {
     path: '/Signup',
     name: 'Signup',
-    component: () => import ('@/views/Signup.vue')
+    component: () => import('@/views/Signup.vue'),
   },
   {
     path: '/Dashboard',
     name: 'Dashboard',
-    component: () => import ('@/views/Dashboard.vue')
-  }
+    component: () => import('@/views/Dashboard.vue'),
+  },
+  {
+    path: '/Group-name/group-members',
+    name: 'Group-members',
+    component: () => import('@/views/Members.vue'),
+  },
 ]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: routeList
+  routes: routeList,
 })
 
 export default router
